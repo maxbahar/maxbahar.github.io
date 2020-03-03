5e2cc6343bafd7c38355478ddbc531972d1111c5
maxbahar <max.a.bahar@gmail.com>
2020-03-03 12:48:07 -0500

    Updates

---
+++ precache-manifest.f413ddfb083b852bc80a2955a8936861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671ccdcebe3143f4820044e548eb66ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61f8d6ac2c59d50f479b",
+    "url": "/static/css/main.e472754d.chunk.css"
+  },
+  {
+    "revision": "c17fa69cf53b3236ebff",
+    "url": "/static/js/2.e564754b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e564754b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f8d6ac2c59d50f479b",
+    "url": "/static/js/main.8b2102e3.chunk.js"
+  },
+  {
+    "revision": "9e9d28e8ac91cb6cb24c",
+    "url": "/static/js/runtime-main.305f59b3.js"
+  }
+]);
