0613363d8cce9ec34aa0869a435d5401e464868b
maxbahar <max.a.bahar@gmail.com>
2020-03-03 19:12:02 -0500

    Updates

---
+++ precache-manifest.3a4ac9adf80ac45fef2889bdbd0a6d28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5773a5cde953d090bdf433eba454fc9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45c8e8b81294d7053961",
+    "url": "/static/css/main.8f5d77cd.chunk.css"
+  },
+  {
+    "revision": "c17fa69cf53b3236ebff",
+    "url": "/static/js/2.e564754b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e564754b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c8e8b81294d7053961",
+    "url": "/static/js/main.9122c9a2.chunk.js"
+  },
+  {
+    "revision": "9e9d28e8ac91cb6cb24c",
+    "url": "/static/js/runtime-main.305f59b3.js"
+  }
+]);
--- src/utils/publicUrl.js
+++
@@ -1,3 +0,0 @@
-export default (assetPath)=>{
-    return process.env.PUBLIC_URL+assetPath;
-}
